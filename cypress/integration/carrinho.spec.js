ec60c3f2d3e3ae868235c72c01eb3191ab37f80e
Vitor <vitorlacerdafaria7@gmail.com>
2021-11-18 13:38:47 -0300

    Adicionado o teste de carrinho. Final.

---
+++ cypress/integration/carrinho.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+context('Teste do carrinho de compras', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/product/abominable-hoodie/')
+    });
+
+    it('Deve adicionar e remover produtos do carrinho', () => {
+
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type('5')
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', '5')
+
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
+
+        cy.get('.remove > .fa').click()
+        cy.get('.woocommerce-message').should('contain', 'removido. Desfazer?')
+    
+    });
+
+});
